fb168c5026fd0865e06c1f446fddbe1ccd88722c
Chandra Gopal <111636625+chandra-learner@users.noreply.github.com>
2022-08-20 19:14:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.title("Welcome to Chandra's Streamlit App")
 st.text("How can I help you?")
+
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
