83e3ff62edad6961af93af17bbd87b7058f282d1
Chandra Gopal <111636625+chandra-learner@users.noreply.github.com>
2022-08-20 19:29:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.title("Welcome to Chandra's Streamlit App")
 st.text("How can I help you?  🥑")
 
@@ -8,3 +9,6 @@ st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
